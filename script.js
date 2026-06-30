ccb628537872497e26c19520574d1b7f5ecff0b9
LeticiaFernandes434 <leticia.silva2456@etec.sp.gov.br>
2026-06-30 20:29:17 -0300

    atualização 3

--- script.js
+++ script.js
@@ -1,1 +1,41 @@
-const s=document.querySelectorAll('.slide');let i=0;function m(){s.forEach(x=>x.classList.remove('active'));s[i].classList.add('active')}document.querySelector('.next').onclick=()=>{i=(i+1)%s.length;m()};document.querySelector('.prev').onclick=()=>{i=(i-1+s.length)%s.length;m()};setInterval(()=>{i=(i+1)%s.length;m()},5000)
+const s = document.querySelectorAll(".slide");
+
+let i = 0;
+
+function m() {
+
+    s.forEach(x => x.classList.remove("active"));
+
+    s[i].classList.add("active");
+
+    // Sempre volta para o topo quando trocar de foto
+    window.scrollTo({
+        top: 0,
+        behavior: "smooth"
+    });
+
+}
+
+document.querySelector(".next").onclick = () => {
+
+    i = (i + 1) % s.length;
+
+    m();
+
+}
+
+document.querySelector(".prev").onclick = () => {
+
+    i = (i - 1 + s.length) % s.length;
+
+    m();
+
+}
+
+setInterval(() => {
+
+    i = (i + 1) % s.length;
+
+    m();
+
+}, 5000);
